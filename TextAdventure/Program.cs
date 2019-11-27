4283cccea0e48dd86cca5567cfbc75cddcc2f5ed
ArtyTri <art_tri@outlook.com>
2019-11-26 19:12:41 -0600

    Finished branching story for Pause method

--- TextAdventure/Program.cs
+++ TextAdventure/Program.cs
@@ -47,14 +47,36 @@ namespace TextAdventure
             }
             else if (resp2 == "overpower")
             {
-                Console.WriteLine("You start out with a quick jab to the face of the robot judge.\nYour hand breaks against the metal and the judge snaps your neck. Oh dear, you are dead!");
+                Console.WriteLine("You start out with a quick jab to the face of the robot judge.\nYour hand breaks against the metal and the judge snaps your neck.\nOh dear, you are dead!");
             }
             Ending();
         }
 
         public void Pause()
         {
-            Console.WriteLine("You freeze time.\nYou die.");
+            string resp3;
+            Console.WriteLine("You freeze time.\nAll is quiet.\nNo wind.\nNo birds chirping.\nNo cars honking.");
+            Console.WriteLine("Out of the nothing you hear a cry for help not too far away.\nThe cry is deafening and gets louder as you approach the area.");
+            Console.WriteLine("You come across an old man with his leg stuck under and time frozen fallen tree.\nYou heave it off of him.");
+            Console.WriteLine("He stands to thank you and offers you three choices for your reward.");
+            Console.WriteLine("Will you choose:\nMore money.\nSuper strength.\nX-Ray vision.");
+            resp3 = Console.ReadLine();
+            if (resp3 == "money")
+            {
+                Console.WriteLine("Time start back up and you get an alert on your phone.\nYou are now 50 million dollars richer.");
+                Console.WriteLine("You get another alert.\nYou bank account is also now temporarily frozen for suspicious activity.\nGood luck explaining this one.");
+            }
+            else if (resp3 == "strength")
+            {
+                Console.WriteLine("Your muscles inflate to tremendous size.\nWithin seconds your pass-out due to loss of blood circulation to your brain as it is all going into your biceps.");
+                Console.WriteLine("You die, frozen in time, a mound of muscle unable to move, but with damn sweet abs.");
+            }
+            else if (resp3 == "x-ray")
+            {
+                Console.WriteLine("To have x-ray vision your eyes have to be able to emit and then receive legitimate rays of radioactive light.");
+                Console.WriteLine("This quickly causes cancerous mutations in everything you look at.\nCongrats x-ray vision turned out to be cancer rays.\nYou are now the worst super villian ever.");
+
+            }
             Ending();
         }
 
